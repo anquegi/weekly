846d63c0e99b7e7f134bf319d31ce6e2d3e33f2f
anquegi <antonio.querol@cuaqea.com>
2020-02-06 09:21:31 +0100

    add ert project structure

---
+++ test/weekly-test.el
@@ -0,0 +1,7 @@
+;;; weekly-test.el --- Tests for weekly
+
+(ert-deftest hello-world ()
+  "Tests working Hello World."
+  (should (equal "Hello World" "Hello World")))
+
+;;; weekly-test.el ends here
